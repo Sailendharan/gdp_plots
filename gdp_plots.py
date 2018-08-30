5a07b0b1d81e4e8215c63a806627f86046957571
Sailendharan Sudakaran <sudakaran@wisc.edu>
2018-08-30 14:03:15 -0500

    takes command line argument for data file

--- gdp_plots.py
+++ gdp_plots.py
@@ -2,13 +2,14 @@ import pandas
 # we need to import part of matplotlib
 # because we are no longer in a notebook
 import matplotlib.pyplot as plt
+import sys
 
 # load data and transpose so that country names are
 # the columns and their gdp data becomes the rows
 
 # read data into a pandas dataframe and transpose
-filename = 'gapminder_gdp_oceania.csv'
-data = pandas.read_csv('gapminder_gdp_oceania.csv', index_col = 'country').T
+filename = sys.argv[1]
+data = pandas.read_csv(filename, index_col = 'country').T
 
 # create a plot the transposed data
 ax = data.plot(title = filename)
